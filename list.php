b624a7e6b28a069c4ec04cc4d0951c72093c3599
getPoland <36799117+getPoland@users.noreply.github.com>
2018-09-07 13:03:01 +0200

    Updated the style of list.php
    BetaSystem got a new style so i updatet the list.php to nerly the same style.
    
    
    -- there are no other changes but if you got some ideas, send me them!

--- list.php
+++ list.php
@@ -10,34 +10,35 @@ $username = "admin";
   <head>
     <meta charset="utf-8">
     <title>Betazugang freischalten</title>
-    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://unpkg.com/papercss@1.4.1/dist/paper.min.css">
   </head>
   <body>
     <h1>Betakeys</h1>
     <h3>Solltest du ein Beta Key besitzen kannst du ihn hier kopieren um dein Betazugang freizuschalten.</h3>
     <br>
-   <?php
- 
-  if($arg1 != null){
-      if($arg2 == $password && $arg1 == $username){
-          include('datenbank.php');
-          $sql = "SELECT * FROM `keys`";
-          echo '<br/>';
-           foreach ($mysqli->query($sql) as $row) {
-               echo "<h3>" .$row['KEY'] . "<br /> </h3>";
-           }
-       }else{      
-           echo '<h3>Username und Passwort stimme nicht überein!</h3>';
-       }  
-   }else{
-       echo '
-       <form method="post" action="list.php">
-       <input type="text" name="user" placeholder="Username" required>
-       <input type="password" name="pass" placeholder="password" required>
-       <input type="submit" value="Login" class="button">
-       </form>';
-   }
-   ?>
+	<?php
+	
+	if($arg1 != null){
+		if($arg2 == $password && $arg1 == $username){
+			include('datenbank.php');
+			$sql = "SELECT * FROM `keys`";
+			echo '<br/>';
+			foreach ($mysqli->query($sql) as $row) {
+				echo "<h3>" .$row['KEY'] . "<br /> </h3>";
+			}
+		}else{		
+			echo '<h3>Username und Passwort stimme nicht überein!</h3>';
+		}	
+	}else{
+		echo '
+		<form method="post" action="list.php">
+		<input type="text" name="user" placeholder="Username" required>
+		<input type="password" name="pass" placeholder="password" required>
+		<input type="submit" value="Login" class="button">
+		</form>';
+	}
+
+
+	?>
   </body>
 </html>
